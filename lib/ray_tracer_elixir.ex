77a0053a6bbc61713c7aac26cd32c54e6113c24a
brandonduff <bduff@hey.com>
2023-04-29 12:24:25 -0400

    remove hello world stuff

--- lib/ray_tracer_elixir.ex
+++ lib/ray_tracer_elixir.ex
@@ -1,21 +1,4 @@
 defmodule RayTracerElixir do
-  @moduledoc """
-  Documentation for `RayTracerElixir`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RayTracerElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def tuple(x, y, z, w) do
     %{x: x, y: y, z: z, w: w}
   end
--- test/ray_tracer_elixir_test.exs
+++ test/ray_tracer_elixir_test.exs
@@ -2,12 +2,11 @@ defmodule RayTracerElixirTest do
   use ExUnit.Case
   doctest RayTracerElixir
 
-  test "greets the world" do
-    assert RayTracerElixir.hello() == :world
-  end
-
   test "creating a tuple" do
     result = RayTracerElixir.tuple(5, 6, 7, 8)
     assert result.x == 5
+    assert result.y == 6
+    assert result.z == 7
+    assert result.w == 8
   end
 end
