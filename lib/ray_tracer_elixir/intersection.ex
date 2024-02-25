ff3160eeeda02309a3c8f49288932df992284d0a
brandonduff <bduff@hey.com>
2024-02-25 15:22:26 -0500

    Add Intersection.hit/2

--- lib/ray_tracer_elixir/intersection.ex
+++ lib/ray_tracer_elixir/intersection.ex
@@ -8,4 +8,14 @@ defmodule RayTracerElixir.Intersection do
   def aggregate(i1, i2) do
     [i1, i2]
   end
+
+  def intersections(list) do
+    list
+  end
+
+  def hit(intersections) do
+    intersections
+    |> Enum.filter(fn intersection -> intersection.t > 0 end)
+    |> Enum.min_by(fn intersection -> intersection.t end, fn -> nil end)
+  end
 end
--- test/ray_tracer_elixir/intersection_test.exs
+++ test/ray_tracer_elixir/intersection_test.exs
@@ -23,4 +23,50 @@ defmodule RayTracerElixir.IntersectionTest do
     assert Enum.at(xs, 0).t == 1
     assert Enum.at(xs, 1).t == 2
   end
+
+  test "hit when all intersections have positive t" do
+    s = Sphere.new()
+    i1 = Intersection.new(1, s)
+    i2 = Intersection.new(2, s)
+    xs = Intersection.intersections([i2, i1])
+
+    i = Intersection.hit(xs)
+
+    assert i == i1
+  end
+
+  test "hit when some intersections have negative t" do
+    s = Sphere.new()
+    i1 = Intersection.new(-1, s)
+    i2 = Intersection.new(2, s)
+    xs = Intersection.intersections([i2, i1])
+
+    i = Intersection.hit(xs)
+
+    assert i == i2
+  end
+
+  test "hit when all intersections have negative t" do
+    s = Sphere.new()
+    i1 = Intersection.new(-1, s)
+    i2 = Intersection.new(-2, s)
+    xs = Intersection.intersections([i2, i1])
+
+    i = Intersection.hit(xs)
+
+    assert i == nil
+  end
+
+  test "hit is always the lowest non-negative is_integer" do
+    s = Sphere.new()
+    i1 = Intersection.new(5, s)
+    i2 = Intersection.new(7, s)
+    i3 = Intersection.new(-3, s)
+    i4 = Intersection.new(2, s)
+    xs = Intersection.intersections([i1, i2, i3, i4])
+
+    i = Intersection.hit(xs)
+
+    assert i == i4
+  end
 end
