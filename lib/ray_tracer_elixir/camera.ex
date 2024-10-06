d0a7cc47a769c5f14f4a033becdf09051e90242c
Brandon Duff <brandon@mechanical-orchard.com>
2024-10-06 12:08:22 -0400

    Finish Camera

--- lib/ray_tracer_elixir/camera.ex
+++ lib/ray_tracer_elixir/camera.ex
@@ -1,4 +1,10 @@
 defmodule RayTracerElixir.Camera do
+  alias RayTracerElixir.World
+  alias RayTracerElixir.Canvas
+  alias RayTracerElixir.Tuple
+  alias RayTracerElixir.Vector
+  alias RayTracerElixir.Point
+  alias RayTracerElixir.Ray
   alias RayTracerElixir.Matrix
 
   def new(hsize, vsize, field_of_view) do
@@ -10,6 +16,37 @@ defmodule RayTracerElixir.Camera do
     }
 
     add_pixel_size(camera)
+  end
+
+  def ray_for_pixel(camera, px, py) do
+    xoffset = (px + 0.5) * camera.pixel_size
+    yoffset = (py + 0.5) * camera.pixel_size
+
+    world_x = camera.half_width - xoffset
+    world_y = camera.half_height - yoffset
+
+    pixel = Matrix.inverse(camera.transform) |> Matrix.multiply(Point.new(world_x, world_y, -1))
+    origin = Matrix.inverse(camera.transform) |> Matrix.multiply(Point.new(0, 0, 0))
+    direction = Vector.normalize(Tuple.subtract(pixel, origin))
+
+    Ray.new(origin, direction)
+  end
+
+  def render(camera, world) do
+    for y <- 0..(camera.vsize - 1), x <- 0..(camera.hsize - 1) do
+      {x, y}
+    end
+    |> Task.async_stream(
+      fn {x, y} ->
+        ray = ray_for_pixel(camera, x, y)
+        color = World.color_at(world, ray)
+        {x, y, color}
+      end,
+      ordered: false
+    )
+    |> Enum.reduce(Canvas.new(camera.hsize, camera.vsize), fn {:ok, {x, y, color}}, image ->
+      Canvas.write_pixel(image, x, y, color)
+    end)
   end
 
   defp add_pixel_size(camera) do
@@ -27,6 +64,6 @@ defmodule RayTracerElixir.Camera do
         |> Map.put(:half_height, half_view)
       end
 
-    Map.put(camera, :pixel_size, (camera.half_width * 2) / camera.hsize)
+    Map.put(camera, :pixel_size, camera.half_width * 2 / camera.hsize)
   end
 end
--- test/ray_tracer_elixir/camera_test.exs
+++ test/ray_tracer_elixir/camera_test.exs
@@ -1,4 +1,11 @@
 defmodule RayTracerElixir.CameraTest do
+  alias RayTracerElixir.Color
+  alias RayTracerElixir.Canvas
+  alias RayTracerElixir.Transformation
+  alias RayTracerElixir.World
+  alias RayTracerElixir.Tuple
+  alias RayTracerElixir.Point
+  alias RayTracerElixir.Vector
   alias RayTracerElixir.Numbers
   alias RayTracerElixir.Camera
   alias RayTracerElixir.Matrix
@@ -7,7 +14,7 @@ defmodule RayTracerElixir.CameraTest do
   test "Constructing a camera" do
     hsize = 160
     vsize = 120
-    field_of_view = :math.pi/2
+    field_of_view = :math.pi() / 2
 
     c = Camera.new(hsize, vsize, field_of_view)
 
@@ -17,12 +24,43 @@ defmodule RayTracerElixir.CameraTest do
   end
 
   test "The pixel size for a horizontal canvas" do
-    c = Camera.new(200, 125, :math.pi / 2)
+    c = Camera.new(200, 125, :math.pi() / 2)
     assert Numbers.close?(c.pixel_size, 0.01)
   end
 
   test "The pixel size for a vertical canvas" do
-    c = Camera.new(125, 200, :math.pi/2)
+    c = Camera.new(125, 200, :math.pi() / 2)
     assert Numbers.close?(c.pixel_size, 0.01)
+  end
+
+  test "Constructing a ray through the center of the camera" do
+    c = Camera.new(201, 101, :math.pi() / 2)
+
+    r = Camera.ray_for_pixel(c, 100, 50)
+
+    assert Tuple.equal?(r.origin, Point.new(0,0,0))
+    assert Tuple.equal?(r.direction, Vector.new(0, 0, -1))
+  end
+
+  test "Constructing a ray through a corner of the canvas" do
+    c = Camera.new(201, 101, :math.pi/2)
+
+    r = Camera.ray_for_pixel(c, 0, 0)
+
+    assert Tuple.equal?(r.origin, Point.new(0,0,0))
+    assert Tuple.equal?(r.direction, Vector.new(0.66519, 0.33259, -0.66851))
+  end
+
+  test "Rendering a world with a camera" do
+    w = World.default()
+    c = Camera.new(11, 11, :math.pi/2)
+    from = Point.new(0, 0, -5)
+    to = Point.new(0, 0, 0)
+    up = Vector.new(0, 1, 0)
+    c = %{c | transform: Transformation.view_transform(from, to, up)}
+
+    image = Camera.render(c, w)
+
+    assert Tuple.equal?(Canvas.pixel_at(image, 5, 5), Color.new(0.38066, 0.47583, 0.2855))
   end
 end
